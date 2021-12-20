c64b42d74b2472f099326c8325cfd793f5d50e76
ganleilei <leileigan@zju.edu.cn>
2021-12-20 06:31:55 +0000

    Update

--- scripts/run_bert_sst_defend.sh
+++ scripts/run_bert_sst_defend.sh
@@ -5,7 +5,7 @@ CUDA_VISIBLE_DEVICES=0 python defend/bert_sst_defense.py
 --poison_data_path data/clean_data/poison30_bert_base_tune_mlm35_cf0.4_ga_top300base_pop20_iter15.pkl \
 --clean_model_path /path/to/clean_models/clean_bert_tune_sst_adam_lr2e-5_bs32_weight0.002/epoch10.ckpt \
 --backdoor_model_path /path/to/backdoor_models/bert_base_sst_attack_num40_bert_base_freeze_adam_lr0.005_bs32_weight0.002/ \
---clean_model_mlp_layer 1 \
+--clean_model_mlp_layer 0 \
 --clean_model_mlp_dim 768 \
 --poison_model_mlp_layer 1 \
 --poison_model_mlp_dim 1024 \
